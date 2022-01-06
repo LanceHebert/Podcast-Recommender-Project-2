77a9b9f78b5328f2f95774a0f18f6f8f2fb77d86
Lance Hebert <LSUHEBERT@gmail.com>
2022-01-06 02:22:48 -0800

    Added embedded player using spotify

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -19,9 +19,14 @@ function EpisodeCard({ episode }) {
         {Math.round(episode.duration_ms / 1000 / 60)} min{" "}
       </span>
       <span className="episode_playback">
-        <video controls width="200">
-          <source src={episode.audio_preview_url} type="audio/mp3"></source>
-        </video>
+        <iframe
+          src={`https://open.spotify.com/embed/episode/${episode.id}?utm_source=generator&theme=0`}
+          width="250"
+          height="152"
+          frameBorder="0"
+          allowfullscreen=""
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        ></iframe>
       </span>
       <span>
         <img alt="upvotes" src="../upvote.png" className="episode_upvotes" />
